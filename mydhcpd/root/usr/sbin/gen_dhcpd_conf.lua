68a2bdb35996cd1f03c279cccec91b686198d31a
helloshiki <aquariusye@163.com>
2015-10-15 17:26:44 +0800

    modify ucitrack

--- mydhcpd/root/usr/sbin/gen_dhcpd_conf.lua
+++ mydhcpd/root/usr/sbin/gen_dhcpd_conf.lua
@@ -217,3 +217,19 @@ io.output(conf)
 io.write(dhcpd_conf_str)
 io.close(conf)
 
+local trackpath = "/etc/config/ucitrack"
+local fp = io.open(trackpath, "rb")
+if fp then 
+	local s = fp:read("*a")
+	fp:close()
+	if s:find("'dnsmasq'") then 
+		s = s:gsub("'dnsmasq'", "'dhcpd'")
+		local tmp = "/tmp/tmp_ucitrack"
+		local fp = io.open(tmp, "wb")
+		fp:write(s)
+		fp:flush()
+		fp:close()
+		os.execute(string.format("mv %s %s", tmp, trackpath))
+	end 
+end 
+
