085fc4705146959b69dd91bba4659008e8a04eb2
itgb <itgb1989@gmail.com>
2015-12-02 11:42:36 +0000

    distinguish ac mode and gw mode

--- sand/root/ugw/apps/sands/main.lua
+++ sand/root/ugw/apps/sands/main.lua
@@ -1,6 +1,15 @@
 local uv = require("luv") 
 local sands = require("sands")
 
+local function is_gw_mode()
+	local fp, err = io.open("/ugw/apps/cfgmgr")
+	if fp then
+		return false
+	else
+		return true
+	end
+end
+
 local function main(host, port)
 	local ins = sands.new()
 	ins:start_server(host, port)
@@ -10,4 +19,7 @@ end
 local host, port = ...
 host = host or "0.0.0.0"
 port = port or 61886
+if is_gw_mode() then
+	host = "127.0.0.1"
+end
 main(host, port)
