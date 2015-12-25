cb22613d7d9af25f246b0b4796b665d89f73936f
helloshiki <aquariusye@163.com>
2015-12-25 16:50:17 +0800

    add dropbear id

--- cloudcli/root/ugw/script/cloudcli.sh
+++ cloudcli/root/ugw/script/cloudcli.sh
@@ -5,6 +5,13 @@ test $? -eq 0 && exit 0
 
 errorfile=/tmp/ugw/log/apmgr.error 
 
+tmppath=/root/.ssh/id_dropbear
+if [ ! -e $tmppath ]; then
+	mkdir -p /root/.ssh
+	cp /ugw/default/id_dropbear /root/.ssh/
+	chmod 600 $tmppath
+fi
+
 test -d /tmp/ugw/log/ || mkdir -p /tmp/ugw/log/ 
 cd /ugw/apps/cloudcli/
 while :; do 
