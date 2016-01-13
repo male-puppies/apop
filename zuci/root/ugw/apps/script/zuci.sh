28baf0dfb8fde7aa13c02599a3d6fecf2fc860d8
helloshiki <aquariusye@163.com>
2016-01-13 15:11:46 +0800

    uci -> zuci

--- zuci/root/ugw/apps/script/zuci.sh
+++ zuci/root/ugw/apps/script/zuci.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-ps | grep uci/main.lua | grep -v grep >/dev/null 
+ps | grep zuci/main.lua | grep -v grep >/dev/null 
 test $? -eq 0 && exit 0
 
 errorfile=/tmp/ugw/log/apmgr.error 
 
 test -d /tmp/ugw/log/ || mkdir -p /tmp/ugw/log/ 
-cd /ugw/apps/uci/
+cd /ugw/apps/zuci/
 while :; do 
-	lua /ugw/apps/uci/main.lua >/dev/null 2>>$errorfile
+	lua /ugw/apps/zuci/main.lua >/dev/null 2>>$errorfile
 	sleep 2
 done
 
