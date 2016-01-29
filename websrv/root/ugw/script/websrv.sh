ba0443c6d8713635d05f4a2232889f1df20b25aa
helloshiki <aquaruisye@163.com>
2016-01-29 16:16:56 +0800

    check webui

--- websrv/root/ugw/script/websrv.sh
+++ websrv/root/ugw/script/websrv.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+diskrom=/www/rom
+diskrom4ac=/www/rom4ac
+
+rm -rf $diskrom
+rm -rf $diskrom4ac
+
+romdir=/tmp/firmware/rom
+rom4acdir=/tmp/firmware/rom4ac
+
+mkdir -p $romdir
+mkdir -p $rom4acdir
+
+ln -sf $romdir $diskrom
+ln -sf $rom4acdir $diskrom4ac
 
 lua /ugw/apps/userauth/adchk.lua &
