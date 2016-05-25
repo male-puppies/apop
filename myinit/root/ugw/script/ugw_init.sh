43216f4ac5e1630179e4bed26b87e38ae1bbfdb7
Ivan <itgb1989@gmail.com>
2016-05-25 15:25:46 +0800

    BUG443:add directory for firmware

--- myinit/root/ugw/script/ugw_init.sh
+++ myinit/root/ugw/script/ugw_init.sh
@@ -1,1 +1,6 @@
 #!/bin/sh
+mkdir -p /tmp/www/webui/
+mkdir -p /tmp/firmware/rom
+mkdir -p /tmp/firmware/rom4
+ln -s /tmp/firmware/rom /tmp/www/webui/rom
+ln -s /tmp/firmware/rom4 /tmp/www/webui/rom4ac
