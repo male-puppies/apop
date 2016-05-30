c524fbf2ee4f0c094846eb49befdaf29f9c1a9b4
Ivan <itgb1989@gmail.com>
2016-05-30 15:55:01 +0800

    BUG443:fix firmware path

--- myinit/root/ugw/script/ugw_init.sh
+++ myinit/root/ugw/script/ugw_init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir -p /tmp/www/webui/
 mkdir -p /tmp/firmware/rom
-mkdir -p /tmp/firmware/rom4
+mkdir -p /tmp/firmware/rom4ac
 ln -s /tmp/firmware/rom /tmp/www/webui/rom
-ln -s /tmp/firmware/rom4 /tmp/www/webui/rom4ac
+ln -s /tmp/firmware/rom4ac /tmp/www/webui/rom4ac
 ln -s /tmp/firmware/rom /www/rom
-ln -s /tmp/firmware/rom4 /www/rom4ac
+ln -s /tmp/firmware/rom4ac /www/rom4ac
